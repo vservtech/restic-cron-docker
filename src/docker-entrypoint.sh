8eda27a5891804a9b659e9f506f24cb200ad52a1
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-09-24 23:48:13 +0200

    update entrypoint script to support LOG_PREFIX variable for maintenance and run a "prestart" script, if available

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 
+LOG_PREFIX="ENTRYPOINT SCRIPT"
+
 # 1) Resolve desired UID/GID
 # Pass the env vars HOST_UID/HOST_GID to the container, BUT DO NOT run the container with the specified user! 
 # aka no "user: ${HOST_UID}:${HOST_GID}" in compose.yaml
@@ -12,16 +14,16 @@ DESIRED_GID="${HOST_GID:-$(id -g)}"
 DESIRED_USER="${HOST_USER:-appuser}"
 DESIRED_GROUP="${HOST_GROUP:-appgroup}"
 
-echo "ENTRY DEBUG: Running with UID: ${DESIRED_UID}"
-echo "ENTRY DEBUG: Running with GID: ${DESIRED_GID}"
-echo "ENTRY DEBUG: Running with USER: ${DESIRED_USER}"
-echo "ENTRY DEBUG: Running with GROUP: ${DESIRED_GROUP}"
+echo "${LOG_PREFIX}: Running with UID: ${DESIRED_UID}"
+echo "${LOG_PREFIX}: Running with GID: ${DESIRED_GID}"
+echo "${LOG_PREFIX}: Running with USER: ${DESIRED_USER}"
+echo "${LOG_PREFIX}: Running with GROUP: ${DESIRED_GROUP}"
 
 # 2) Ensure group exists inside the container for DESIRED_GID
 if ! getent group "${DESIRED_GID}" >/dev/null 2>&1 && \
    ! getent group "${DESIRED_GROUP}" >/dev/null 2>&1; then
   if ! addgroup -g "${DESIRED_GID}" "${DESIRED_GROUP}"; then
-    echo "ENTRY ERROR: addgroup failed for GID ${DESIRED_GID} and GROUP ${DESIRED_GROUP}" >&2
+    echo "${LOG_PREFIX}: ERROR: addgroup failed for GID ${DESIRED_GID} and GROUP ${DESIRED_GROUP}" >&2
     exit 1
   fi
 fi
@@ -34,7 +36,7 @@ GROUP_NAME="$(getent group "${DESIRED_GID}" | cut -d: -f1 || true)"
 if ! getent passwd "${DESIRED_UID}" >/dev/null 2>&1 && \
    ! getent passwd "${DESIRED_USER}" >/dev/null 2>&1; then
   if ! adduser -D -u "${DESIRED_UID}" -G "${GROUP_NAME}" "${DESIRED_USER}"; then
-    echo "ENTRY ERROR: adduser failed for UID ${DESIRED_UID} and USER ${DESIRED_USER}" >&2
+    echo "${LOG_PREFIX}: ERROR: adduser failed for UID ${DESIRED_UID} and USER ${DESIRED_USER}" >&2
     exit 1
   fi
 fi  
@@ -48,12 +50,26 @@ if [ -d /opt/cron ]; then
   chown -R "${USER_NAME}:${GROUP_NAME}" /opt/cron
 fi
 
-echo "ENTRY DEBUG: Command passed to docker exec: $*"
+echo "${LOG_PREFIX}: Command passed to docker exec: $*"
 
 # 5) If already correct uid/gid, just exec
 if [ "$(id -u)" = "${DESIRED_UID}" ] && [ "$(id -g)" = "${DESIRED_GID}" ]; then
+
+  # Run prestart script
+  if [ -x /usr/local/bin/prestart ]; then
+    echo "${LOG_PREFIX}: Running prestart script..."
+    /usr/local/bin/prestart
+  fi
+
+  # Run main script
   exec "$@"
 fi
 
 # 6) Exec as that user
+# Run prestart script as the desired user
+if [ -x /usr/local/bin/prestart ]; then
+  echo "${LOG_PREFIX}: Running prestart script..."
+  su-exec "${USER_NAME}:${GROUP_NAME}" /usr/local/bin/prestart
+fi
+# Run main script as the desired user
 exec su-exec "${USER_NAME}:${GROUP_NAME}" "$@"
